de84e110ae4b79b75781a25407a8a52967bdd545
xiaohanfu123 <shawnx1995@gmail.com>
2022-08-22 19:18:10 +0000

    after Eslint

--- lunch.js
+++ lunch.js
@@ -14,51 +14,26 @@
  */
 
 const whatToDoForLunch = function(hungry, availableTime) {
- if(hungry == 1){
-  if(availableTime < 20){ 
-    return 'Grab some snacks'
-  } else if(availableTime >= 20 && availableTime <30){ 
-    return 'Cook a tasty meal '
-  } else if(availableTime>=30){  
-    return 'Get back, this is a intensive program'
-  }
- } else return 'Wait till getting hungry'
-}
-
-
-/*
- * This is some test runner code that's simply calling our whatToDoForLunch function
- * defined above to verify we're making the right decisions. Do not modify it!
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * git remote set-url test xiaohanfu123@github.com:xiaohanfu123/test.git
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- */
+  if (hungry === true) {
+    if (availableTime < 20) {
+      return 'Grab some snacks';
+    } else if (availableTime >= 20 && availableTime < 30) {
+      return 'Cook a tasty meal';
+    } else if (availableTime >= 30) {
+      return 'Get back, this is a intensive program';
+    }
+  } else return 'Wait till getting hungry';
+};
 
 console.log("I'm hungry and I have 20 minutes for lunch.");
 console.log(whatToDoForLunch(true, 20));
-console.log('--')
+console.log('--');
 
 console.log("I'm hungry and I have 50 minutes for lunch.");
 console.log(whatToDoForLunch(true, 50));
-console.log('--')
+console.log('--');
 console.log("I'm not hungry and I have 30 minutes for lunch.");
 console.log(whatToDoForLunch(false, 30));
-console.log('--')
+console.log('--');
 console.log("I'm hungry and I have 15 minutes for lunch.");
 console.log(whatToDoForLunch(true, 15));
